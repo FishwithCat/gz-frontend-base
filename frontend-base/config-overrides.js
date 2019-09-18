088492aa8e2adc6e15c55a095e527e5fe4f94881
liuyz <liuyz@agilean.cn>
2019-09-19 00:28:30 +0800

    配置gz项目基座

---
+++ frontend-base/config-overrides.js
@@ -0,0 +1,25 @@
+const { override, fixBabelImports } = require('customize-cra');
+
+const externals = (config) => {
+    config.optimization.runtimeChunk = false;
+    config.optimization.splitChunks = {
+        cacheGroups: {
+        default: false,
+        },
+    }
+
+    config.externals = {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    }
+    return config;
+}
+
+module.exports = override(
+    externals,
+    fixBabelImports('import', {
+        libraryName: 'element-ui',
+        libraryDirectory: 'es',
+        style: 'element-ui',
+    })
+);
